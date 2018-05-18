705f281811cc18458bac8c552e226ee7a8ba9448
MggMuggins <mggmugginsmc@gmail.com>
2018-05-17 21:15:35 -0500

    My error situation

--- src/main/kotlin/org/terasology/KotlinCommands.kt
+++ src/main/kotlin/org/terasology/KotlinCommands.kt
@@ -4,13 +4,11 @@ import org.terasology.entitySystem.systems.BaseComponentSystem
 import org.terasology.entitySystem.systems.RegisterSystem
 import org.terasology.logic.console.commandSystem.annotations.Command
 
-
 @RegisterSystem
 class KotlinCommands : BaseComponentSystem () {
 
   @Command(shortDescription = "Kotlin test command", runOnServer = false)
-  fun testKotlin(): kotlin.String {
-      val str = "Kotlin ran successfully!"
-      return str
+  fun testKotlin(): String {
+      return "Kotlin runs!"
   }
 }
